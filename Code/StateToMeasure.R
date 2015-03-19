3642458b6e2999b60918f07b8dffa73cf42a4ca7
dengdetian0603 <dengdetian0603@gmail.com>
2015-03-19 17:01:57 -0400

    Generate measurements Y from true state L

--- Code/StateToMeasure.R
+++ Code/StateToMeasure.R
@@ -1,4 +1,14 @@
+library(doMC)
+library(foreach)
+
 LtoY = function(L,TPR,FPR)
 {
-      
+      k = ncol(L)
+      notL = 1-L
+      registerDoMC(4) 
+      Y= foreach(i=1:nrow(L), .combine=rbind) %dopar% {
+           rbinom(k,1,L[i,]*TPR+notL[i,]*FPR)
+      }
+      Y
 }
+
--- Code/tester.R
+++ Code/tester.R
@@ -6,21 +6,27 @@ library(foreach)
 library(MVB)
 source("~/Documents/Johns Hopkins SPH/Research/S.Zeger/LogLinearModel/Code/LS.model.R")
 source("~/Documents/Johns Hopkins SPH/Research/S.Zeger/LogLinearModel/Code/LL.exch_150217.R")
+source("~/Documents/Johns Hopkins SPH/Research/S.Zeger/LogLinearModel/Code/StateToMeasure.R")
 registerDoMC(4) 
 
 Pis = c(0.1,0.5,0.3,0.05,0.04,0.009,0.001-1e-5,1e-5)
 K = length(Pis)-1
 
-n = 200; m = 5000
+n = 500; m = 50000
 
 # sim strategy 1
 L = mvb.exch.sim(Pis, n)
 # sim strategy 2
-theta0 = matrix(c(rnorm(K,-0.3,1.2),rnorm(choose(K,2),-1,1.5)),nrow=1)
+theta0 = matrix(c(rnorm(K,-0.3,1),rnorm(choose(K,2),-0.5,1.5)),nrow=1)
 x1 = matrix(rep(1,1*n),nrow=n)
 L = mvb.simu(theta0, x1, K, offset = 0)$response
 
 table(factor(apply(L,1,sum),levels=0:K))
+
+tpr = pmin(rnorm(K,0.9,0.1),1)
+fpr = pmax(rnorm(K,0.1,0.05),0)
+Y = LtoY(L,tpr,fpr) # generating Y with TPR and FPR 
+L=Y
 
 #-- expand the observed vectors
 L.expand = foreach(i=1:nrow(L), .combine=rbind) %dopar% {
@@ -52,7 +58,7 @@ beta2 = fit2$par
 
 
 
-plot(beta1) # black: L model
+plot(beta1,ylim=c(min(beta1,beta2,theta0[1,]),max(beta1,beta2,theta0[1,]))) # black: L model
 points(beta2,col="blue") # blue: L + S model
 points(as.vector(theta0),col="red") # red: true value
 
