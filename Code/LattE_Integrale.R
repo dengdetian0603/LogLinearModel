0cdfbe46abaaace035b6d4392519f4d06ffd7e8f
dengdetian0603 <dengdetian0603@gmail.com>
2015-11-04 10:06:08 -0500

    sync before edit

--- Code/LattE_Integrale.R
+++ Code/LattE_Integrale.R
@@ -1,63 +1,73 @@
 source("/home/bst/student/ddeng/ThesisTopic/LogLinearModel/GitRepo/LogLinearModel/Code/ToolBox_PERCH.R")
 source("/home/bst/student/ddeng/ThesisTopic/LogLinearModel/GitRepo/LogLinearModel/Code/Sparse_Models.R")
-#library(MVB)
-#library(limSolve)
 library(doMC)
 library(foreach)
-#library(coda)
 library(MASS)
-#library(editrules)
+
 
 num_core = min(detectCores(), 12)
 registerDoMC(num_core)
 
 load("SimDat_K5n300.RData")
 
+# ------------------------------------------------------------------------------------------ #
+# call LattE function integrate from R
+#GenMatrices(K=5)$Lmatrix
+
 Lmat = GenMatrices.Sparse(K=5, Smax=3)
 MuMat = Lmat$MuMat
 PiMat = Lmat$PiMat
 J1 = Lmat$J1
-
-
-GenMatrices.Sparse = function(K, Smax=3)
-{
-      L.all = as.matrix(AllComb(K))[-1,]
-      S = apply(L.all,1,sum)
-      index = order(S)
-      L.all = L.all[index,]
-      S = S[index]
-      
-      Sallowed = which(S<=Smax)
-      L.all = L.all[Sallowed,]
-      S = S[Sallowed]
-      J1 = length(S)
-      
-      #-- expand all possible combinations
-      #L.all.exp = foreach(i=1:nrow(L.all), .combine=rbind) %dopar% {
-      #      expandL.LL(L.all[i,])
-      #}
-      
-      MuMat = matrix(NA,nrow=K,ncol=J1)
-      for (i in 1:K)
-      {
-            MuMat[i,] = as.numeric(L.all[,i]==1)
-      }
-      
-      PiMat = matrix(NA,nrow=Smax,ncol=J1)
-      for (i in 1:Smax)
-      {
-            PiMat[i,] = as.numeric(S==i)
-      }
-      return(list(MuMat=MuMat, PiMat=PiMat, J1=J1))
-}
-
-tryCatch(xsample( E = rbind(pars$PiMat,pars$MuMat), F = c(pis[-1],mu)/pis[1], G = diag(rep(1,pars$J1)), H = rep(0,pars$J1),
-                      iter = n, burnin = burnin, type = method, test=FALSE), error = function(e) "Incompatible constraints.")
-
-
-# ------------------------------------------------------------------------------------------ #
-# call LattE function integrate from R
 mu.tmp = c(0.258,0.157,0.165,0.288,0.541)
 pi.tmp = c(0.103,0.513,0.256,0.128)
+
+d = ncol(MuMat)
+Resolution = 1
+ScaleUp = 1000
+
+Equality = round(cbind(c(mu.tmp, pi.tmp[-(1:2)])*ScaleUp, -rbind(MuMat,PiMat[-1,]))*Resolution,0)
+Inequality = round(cbind(rep(1,d)*ScaleUp, -diag(1,d))*Resolution,0)
+# -------------------------------------------------------------- #
+
+b_A = rbind(Equality, Inequality)
+m = nrow(b_A)
+
+
+n_eq = 7
+b_A = rbind(Equality[1:n_eq,], Inequality)
+m = nrow(b_A)
+
+b_A = Inequality/Resolution
+m = nrow(b_A)
+# -------------------------------------------------------------- #
+# write constraint file
+sink("Constraints.txt")
+cat(paste(m,d+1,"\n"))
+for (i in 1:m)
+{
+    cat(c(paste(b_A[i,]),"\n"))     
+}
+cat(c("linearity",paste(c(n_eq,1:n_eq)),"\n"))
+cat(c("nonnegative",paste(c(d,1:d)),"\n"))
+sink()
+
+# -------------------------------------------------------------- #
+# write integrand file
+Integrand = diag(rep(1,d))
+sink("Integrand.txt")
+cat(c("[[1,[1,[", paste(Integrand[i,],collapse=","), "]]]]","\n"))
+sink()
+
+
+PathToLattE = "/home/bst/student/ddeng/latte/bin/integrate"
+
+int_command = "~/latte/bin/integrate --valuation=integrate --triangulate --linear-forms=Integrand.txt Constraints.txt "
+
+vol_command = "~/latte/bin/integrate --valuation=volume --triangulate Constraints.txt"
+vol_command = "~/latte/bin/integrate --valuation=volume --cone-decompose Constraints.txt"
+
+system("cat Constraints.txt")
+system(vol_command, intern = T)
+system(int_command, intern = T)
 
 
